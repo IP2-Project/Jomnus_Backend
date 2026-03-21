fd869bc48ec02dc6656ae46f0cc0b69034943ea1
Axgust22 <cheangseyha2208@gmail.com>
2026-03-21 21:19:37 +0700

    add modules for authentication, users, profiles, tasks, categories, assignments, workflow, proofs, reviews, and telegram

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { ProfilesModule } from './profiles/profiles.module';
+import { TasksModule } from './tasks/tasks.module';
+import { CategoriesModule } from './categories/categories.module';
+import { AssignmentsModule } from './assignments/assignments.module';
+import { WorkflowModule } from './workflow/workflow.module';
+import { ProofsModule } from './proofs/proofs.module';
+import { ReviewsModule } from './reviews/reviews.module';
+import { TelegramModule } from './telegram/telegram.module';
 
 @Module({
-  imports: [],
+  imports: [
+    AuthModule,
+    UsersModule,
+    ProfilesModule,
+    TasksModule,
+    CategoriesModule,
+    AssignmentsModule,
+    WorkflowModule,
+    ProofsModule,
+    ReviewsModule,
+    TelegramModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
